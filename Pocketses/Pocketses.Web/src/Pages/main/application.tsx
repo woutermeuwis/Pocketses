54bd627e4b4087a953c4e6ce79cc096135de8ee6
Wouter Meuwis <wouter.meuwis@3factr.be>
2022-11-16 21:42:56 +0100

    Init dashboard

--- Pocketses/Pocketses.Web/src/Pages/main/application.tsx
+++ Pocketses/Pocketses.Web/src/Pages/main/application.tsx
@@ -1,8 +1,10 @@
-import { Disclosure } from "@headlessui/react";
-import { ReactNode } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Fragment, ReactNode } from "react";
 import { apiRoutes } from "../../api/apiRoutes";
 import { useAuth } from "../../components/contexts/auth-context";
 import { useUser } from "../../components/contexts/user-context";
+import { Bars3Icon, XMarkIcon, BriefcaseIcon } from '@heroicons/react/24/outline'
+
 
 const Application = () => {
 
@@ -13,24 +15,106 @@ const Application = () => {
         http.get(apiRoutes.baseUrl + 'weatherforecast')
     }
 
-    const onOpen = (open) => {
+    const navigation = [
+        { name: 'Dashboard', href: '#', current: true },
+        { name: 'Campaigns', href: '#', current: false },
+        { name: 'Characters', href: '#', current: false }
+    ];
+
+    const userNavigation = [
+        { name: 'Log out', href: '#' }
+    ]
+
+    const profileImg = "https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg"
+
+    const ToNavigationItem = ({ name, href, current }: { name: string, href: string, current: boolean }) => {
+        return current
+            ? (<a key={name} href={href} className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">{name}</a>)
+            : (<a key={name} href={href} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{name}</a>);
+    }
+
+    const ToNavigationMobileButton = ({ name, href, current }: { name: string, href: string, current: boolean }) => {
+        return current
+            ? (<Disclosure.Button as="a" key={name} href={href} className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">{name}</Disclosure.Button>)
+            : (<Disclosure.Button as="a" key={name} href={href} className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{name}</Disclosure.Button>);
+    }
+
+    const ToUserNavigationItem = ({ name, href }: { name: string, href: string }) => {
+        return (<Menu.Item key={name}>
+            {(args) => args.active
+                ? (<a href={href} className="block px-4 py-2 text-sm text-gray-700 bg-gray-100">{name}</a>)
+                : (<a href={href} className="block px-4 py-2 text-sm text-gray-700">{name}</a>)
+            }
+        </Menu.Item>)
+    }
+
+    const ToUserNavigationMobileButton = ({ name, href }: { name: string, href: string }) => {
+        return (<Disclosure.Button as="a" key={name} href={href} className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+            {name}
+        </Disclosure.Button>);
+    }
+
+    const UserNavigationTransition = ({ children }: { children: ReactNode }) => {
+        return (
+            <Transition as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95">
+                {children}
+            </Transition>
+        )
+    }
+
+    const Navbar = () => {
         return (
             <>
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div className="flex h-16 items-center justify-between">
-                        <div className="flex items-center">
-                            
-                            <div className="flex-shrink-0">
-                                <img className="h-8 w-8"
-                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" />
-                            </div>
+                <div className="flex items-center">
 
-                            
+                    {/* icon */}
+                    <div className="flex-shrink-0">
+                        <BriefcaseIcon className="h-8 w-8 text-gray-300" />
+                    </div>
+
+                    {/* nav buttons */}
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            {navigation.map(ToNavigationItem)}
                         </div>
                     </div>
+
                 </div>
+
+                {/* profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+
+                    <Menu.Button className="max-w-xs rounded-full bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <img className="h-8 w-8 rounded-full" src={profileImg} />
+                    </Menu.Button>
+
+                    <UserNavigationTransition>
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {userNavigation.map(ToUserNavigationItem)}
+                        </Menu.Items>
+                    </UserNavigationTransition>
+
+                </Menu>
             </>
-        )
+        );
+    }
+
+    const MobileButton = ({ open }: { open: boolean }) => {
+        return (
+            <div className="-mr-2 flex md:hidden">
+                <Disclosure.Button className="rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    {open
+                        ? (<XMarkIcon className="block h-6 w-6" />)
+                        : (<Bars3Icon className="block h-6 w-6" />)}
+                </Disclosure.Button>
+            </div>
+        );
     }
 
     const ApplicationHeader = ({ children }: { children: ReactNode }) => {
@@ -54,7 +138,38 @@ const Application = () => {
     return (
         <div className="min-h-full">
             <Disclosure as="nav" className="bg-gray-800">
-                {onOpen}
+                {(args) => (
+                    <>
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                            <div className="flex h-16 items-center justify-between">
+                                <Navbar />
+                                <MobileButton open={args.open} />
+                            </div>
+                        </div>
+
+                        <Disclosure.Panel className="md:hidden">
+                            <div className="space-y-1 px-2 pt-2 pb-3 sm:px:3">
+                                {navigation.map(ToNavigationMobileButton)}
+                            </div>
+                            <div className="border-t border-gray-700 pt-4 pb-3">
+                                <div className="flex items-center px-5">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src={profileImg} />
+                                    </div>
+                                    <div className="ml-3">
+                                        <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                                        <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                                    </div>
+                                </div>
+                                <div className="mt-3 space-y-1 px-2">
+                                    {userNavigation.map(ToUserNavigationMobileButton)}
+                                </div>
+                            </div>
+                        </Disclosure.Panel>
+
+
+                    </>
+                )}
             </Disclosure>
 
             <ApplicationHeader>
@@ -67,7 +182,7 @@ const Application = () => {
                 </div>
             </ApplicationContent>
 
-        </div>
+        </div >
     )
 }
 export default Application;
